8c787d6345d00530497f2ccb2ecc3fc291fed997
Ethan Kennerly <ethankennerly@gmail.com>
2019-10-12 08:11:17 -0700

    Go Searcher Tests: First move on 3x3: 1.7 seconds.

--- LudumDare43/Assets/Editor/UnitTests/Go/GoSearcherTests.cs
+++ LudumDare43/Assets/Editor/UnitTests/Go/GoSearcherTests.cs
@@ -1,6 +1,9 @@
 using Go;
 using FineGameDesign.Go;
 using NUnit.Framework;
+using System.Diagnostics;
+
+using Debug = UnityEngine.Debug;
 
 namespace FineGameDesign.UnitTests.Go
 {
@@ -9,13 +12,22 @@ namespace FineGameDesign.UnitTests.Go
         [Test]
         public void FirstMoveOn3x3()
         {
+            Stopwatch timePerMove = new Stopwatch();
+
             Referee referee = new Referee();
             referee.Game = new Game(new Board(3, 3), Content.Black);
             GoSearcher searcher = new GoSearcher();
+
+            timePerMove.Start();
             searcher.MakeMove(referee);
+            timePerMove.Stop();
+            long millisecondsPerMove = timePerMove.ElapsedMilliseconds;
+
             uint x1y1Mask = Board.GetCellMask(1, 1, 3, 3);
             Board board = referee.Game.Board;
             Assert.AreEqual(x1y1Mask, board.GetContentMask(0), board.ToString());
+
+            Debug.Log("FirstMoveOn3x3: " + millisecondsPerMove + "ms");
         }
     }
 }
