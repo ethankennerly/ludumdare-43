9e871a80b50d8213c18ed8eb8ac36ae2a0b6f668
Ethan Kennerly <ethankennerly@gmail.com>
2019-11-24 15:49:41 -0800

    Go Game State 5x5: Coordinate to mask.

--- LudumDare43/Assets/Editor/UnitTests/Go/GoGameState5x5Tests.cs
+++ LudumDare43/Assets/Editor/UnitTests/Go/GoGameState5x5Tests.cs
@@ -11,5 +11,23 @@ namespace FineGameDesign.Go.UnitTests
             GoGameState5x5 gameState = new GoGameState5x5();
             Assert.AreEqual(0, gameState.IllegalMoveMask);
         }
+
+        [Test]
+        public void IllegalMoveMask_AfterMove_EqualsBitShiftYPlusX()
+        {
+            GoGameState5x5 gameState = new GoGameState5x5();
+            uint moveMask = gameState.CoordinateToMask(new BoardPosition(){x = 3, y = 2});
+            gameState.Move(moveMask);
+            Assert.AreEqual(1024 + 3, gameState.IllegalMoveMask);
+        }
+
+        [Test]
+        public void CoordinateToMask_BoardPosition3x2_EqualsToBitShiftYPlusX()
+        {
+            GoGameState5x5 gameState = new GoGameState5x5();
+            uint moveMask = gameState.CoordinateToMask(new BoardPosition(){x = 2, y = 1});
+            gameState.Move(moveMask);
+            Assert.AreEqual(32 + 2, gameState.IllegalMoveMask);
+        }
     }
 }
--- LudumDare43/Assets/Scripts/Go/AI/GoGameState5x5.cs
+++ LudumDare43/Assets/Scripts/Go/AI/GoGameState5x5.cs
@@ -1,7 +1,41 @@
 namespace FineGameDesign.Go
 {
+    /// <summary>
+    /// Constant throughout play.
+    ///
+    /// Public variables for speedy access.
+    /// </summary>
+    public sealed class GoConfig5x5
+    {
+        public int SizeX = 5;
+        public int SizeY = 5;
+    }
+
+    public struct BoardPosition
+    {
+        public int x;
+        public int y;
+    }
+
+    /// <summary>
+    /// Varies throughout play.
+    ///
+    /// Public variables for speedy access.
+    /// </summary>
     public sealed class GoGameState5x5
     {
+        public GoConfig5x5 Config = new GoConfig5x5();
+
         public uint IllegalMoveMask;
+
+        public uint CoordinateToMask(BoardPosition pos)
+        {
+            return (uint)((1 << (Config.SizeY * pos.y)) + pos.x);
+        }
+
+        public void Move(uint moveMask)
+        {
+            IllegalMoveMask |= moveMask;
+        }
     }
 }
